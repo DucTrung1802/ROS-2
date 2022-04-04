eb0bdc3d6695f6ef9c937c8c602636b6a188abba
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-04-04 19:54:56 +0700

    #21 before test 15

--- motor/motor/drive_motor.py
+++ motor/motor/drive_motor.py
@@ -189,7 +189,7 @@ class Publisher(Node):
     def __init__(self):
         super().__init__("minimal_publisher")
         self.publisher_ = self.create_publisher(Twist, TOPIC, 10)
-        timer_period = 0.01  # seconds
+        timer_period = 1 / PUBLISH_FREQUENCY  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
