6b9dd1c8e9fbfc469f827962468406e646f6e176
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-04-20 22:28:36 +0700

    #73 add class SonarNode

--- my_package/sonar/sonar/sonar_node.py
+++ my_package/sonar/sonar/sonar_node.py
@@ -8,17 +8,6 @@ import time
 
 timer1 = time.time()
 
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-
-# set GPIO Pins
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
-
-# set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-
 
 class MinimalPublisher(Node):
     def __init__(self):
@@ -38,34 +27,6 @@ class MinimalPublisher(Node):
         self.publisher_1.publish(msg)
 
 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
-
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-
-    return distance
-
-
 def main(args=None):
     global timer1
     rclpy.init(args=args)
@@ -81,7 +42,3 @@ def main(args=None):
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
-
-
-if __name__ == "__main__":
-    main()
