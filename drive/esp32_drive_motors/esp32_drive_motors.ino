c1e17f60c23ac15ee9473ae6eac3aec41aad1b4b
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-04-26 22:31:16 +0700

    #113 add direction for motors - before test 1

--- drive/esp32_drive_motors/esp32_drive_motors.ino
+++ drive/esp32_drive_motors/esp32_drive_motors.ino
@@ -50,6 +50,7 @@ String serialLine = "";
 // Use arduinojson.org/v6/assistant to compute the capacity.
 StaticJsonDocument<200> JSON_DOC_RECEIVE;
 String KEY[15] = "motor_data";
+volatile int wheel_direction[2] = {0, 0};
 volatile int pwm_frequency[2] = {0, 0};
 volatile int pwm_pulse[2] = {0, 0};
 
@@ -70,20 +71,62 @@ bool deserializeJSON() {
   return true;
 }
 
+void decodeJSON() {
+  wheel_direction[0] = JSON_DOC_RECEIVE["motor_data"][0];
+  pwm_frequency[0] = JSON_DOC_RECEIVE["motor_data"][1];
+  pwm_pulse[0] = JSON_DOC_RECEIVE["motor_data"][2];
+
+  wheel_direction[1] = JSON_DOC_RECEIVE["motor_data"][3];
+  pwm_frequency[1] = JSON_DOC_RECEIVE["motor_data"][4];
+  pwm_pulse[1] = JSON_DOC_RECEIVE["motor_data"][5];
+}
+
+void driveLeftWheel() {
+  if (wheel_direction[0] == 1) {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+  else if (wheel_direction[0] == -1) {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
+  else if (wheel_direction[0] == 0) {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+  }
+}
+
+void driveRightWheel() {
+  if (wheel_direction[1] == 1) {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+  }
+  else if (wheel_direction[1] == -1) {
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+  }
+  else if (wheel_direction[1] == 0) {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
+  }
+}
+
 void drive_motors() {
+  //  JSON form: {"motor_data": [DIRECTION_LEFT, PWM_FREQUENCY_LEFT, PWM_LEFT, DIRECTION_RIGHT, PWM_FREQUENCY_RIGHT, PWM_RIGHT]}
+  //  DIRECTION_RIGHT / DIRECTION_LEFT: 1 is forward, 0 is stop, -1 is backward
+  //  PWM_FREQUENCY_LEFT / PWM_FREQUENCY_RIGHT: 1000 - 100000 Hz
+  //  PWM: 0 - 1023
   if (deserializeJSON()) {
-    pwm_frequency[0] = JSON_DOC_RECEIVE["motor_data"][0];
-    pwm_frequency[1] = JSON_DOC_RECEIVE["motor_data"][2];
-    pwm_pulse[0] = JSON_DOC_RECEIVE["motor_data"][1];
-    pwm_pulse[1] = JSON_DOC_RECEIVE["motor_data"][3];
+    decodeJSON();
+    digitalWrite(STBY, LOW);
+    driveLeftWheel();
+    driveRightWheel();
+    digitalWrite(STBY, HIGH);
+
     ledcSetup(CHANNEL_PWMA, pwm_frequency[0], RESOLUTION_PWM_1);
     ledcSetup(CHANNEL_PWMB, pwm_frequency[1], RESOLUTION_PWM_2);
     ledcWrite(CHANNEL_PWMA, pwm_pulse[0]);
     ledcWrite(CHANNEL_PWMB, pwm_pulse[1]);
-    //    Serial.print("Left wheel pwm: ");
-    //    Serial.println(pwm_pulse[0]);
-    //    Serial.print("Right wheel pwm: ");
-    //    Serial.println(pwm_pulse[1]);
   }
 }
 
