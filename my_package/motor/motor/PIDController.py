82715a653a07b7b8d4865d6fa9b3437980a55b95
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-02 16:27:01 +0700

    #271 add antiWindup feature for PID controller

--- my_package/motor/motor/PIDController.py
+++ my_package/motor/motor/PIDController.py
@@ -7,17 +7,16 @@ class PIDController:
         self.__initializeCoefficient(Kp, Ki, Kd, T, min, max)
 
     def __checkConditions(self, Kp, Ki, Kd, T, min, max):
-        if Kp < 0:
+        if Kp <= 0:
             raise Exception("Kp must not be negative!")
-        if Ki < 0:
+        if Ki <= 0:
             raise Exception("Ki must not be negative!")
-        if Kd < 0:
+        if Kd <= 0:
             raise Exception("Kd must not be negative!")
         if T <= 0:  # T is sample time
             raise Exception("T must be positive!")
         if min > max:
-            raise Exception(
-                "Minimum value must smaller or equal to maximum value!")
+            raise Exception("Minimum value must smaller or equal to maximum value!")
 
     def __applyCoefficients(self, Kp, Ki, Kd, T):
         self.__Kp = Kp
@@ -26,14 +25,18 @@ class PIDController:
         self.__T = T
 
     def __computeCoefficients(self):
-        self.__alpha = 2 * self.__T * self.__Kp + \
-            self.__Ki * self.__T * self.__T + 2 * self.__Kd
-        self.__beta = self.__Ki * self.__T * self.__T - \
-            4 * self.__Kd - 2 * self.__T * self.__Kp
+        self.__alpha = (
+            2 * self.__T * self.__Kp + self.__Ki * self.__T * self.__T + 2 * self.__Kd
+        )
+        self.__beta = (
+            self.__Ki * self.__T * self.__T - 4 * self.__Kd - 2 * self.__T * self.__Kp
+        )
         self.__gamma = 2 * self.__Kd
         self.__delta = 2 * self.__T
 
     def __initializeCoefficient(self, Kp, Ki, Kd, T, min, max):
+
+        self.__Ki_on = Ki
 
         self.__applyCoefficients(Kp, Ki, Kd, T)
 
@@ -57,6 +60,27 @@ class PIDController:
             return self.__min
         else:
             return value
+
+    def __getSign(self, value):
+        if value > 0:
+            return 1
+        elif value < 0:
+            return -1
+        else:
+            return 0
+
+    def __antiWindup(self):
+        self.__is_saturating = self.__uk != self.__saturate(self.__uk)
+        self.__sign_of_error = self.__getSign(self.__ek)
+        self.__sign_of_control_output = self.__getSign(self.__uk)
+        self.__same_sign = self.__sign_of_error == self.__sign_of_control_output
+
+        if self.__is_saturating and self.__same_sign:
+            self.__Ki = 0
+        else:
+            self.__Ki = self.__Ki_on
+
+        self.__computeCoefficients()
 
     def changeCoefficients(self, Kp, Ki, Kd, T):
         if Kp < 0:
@@ -97,6 +121,8 @@ class PIDController:
         self.__ek_2 = self.__ek_1
         self.__ek_1 = self.__ek
         self.__state += 1
+
+        self.__antiWindup()
 
     def getOutputValue(self):
         return self.__saturate(self.__uk)
