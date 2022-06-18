053843b325e5359f1640b3f92a6142223539f3e9
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-18 15:51:57 +0700

    #347 cythonized variables.py

--- fuzzy_logic/fuzzy_logic/variables.py
+++ fuzzy_logic/fuzzy_logic/variables.py
@@ -14,15 +14,15 @@ from .terms import Term
 
 class FuzzyVariable:
 
-    def __init__(self, name: cython.char, min_value: float = 0.0, max_value: float = 1.0, *terms: Term):
+    def __init__(self, name: cython.char, min_value: cython.double = 0.0, max_value: cython.double = 1.0, *terms: Term):
         if min_value >= max_value:
             raise ValueError(f'{min_value} <= {max_value} is not True')
-        self.name: str = name
-        self.min_value: float = min_value
-        self.max_value: float = max_value
+        self.name: cython.char = name
+        self.min_value: cython.double = min_value
+        self.max_value: cython.double = max_value
         self.terms: List[Term] = list(terms)
 
-    def term_by_name(self, name: str) -> Term:
+    def term_by_name(self, name: cython.char) -> Term:
         """
         Find term by name
         :param name: name of term
@@ -43,26 +43,26 @@ class SugenoFunction(ABC):
     """
     @property
     @abstractmethod
-    def name(self) -> str:
+    def name(self) -> cython.char:
         ...
 
     @abstractmethod
-    def evaluate(self, inputs: Dict[FuzzyVariable, float]) -> float:
+    def evaluate(self, inputs: Dict[FuzzyVariable, cython.double]) -> cython.double:
         ...
 
 
 class LinearSugenoFunction(SugenoFunction):
 
-    def __init__(self, name: str, coefficients: Dict[FuzzyVariable, float], const: float = .0):
-        self.__name: str = name
+    def __init__(self, name: str, coefficients: Dict[FuzzyVariable, cython.double], const: cython.double = .0):
+        self.__name: cython.char = name
         self.coefficients: Dict[FuzzyVariable, float] = coefficients
-        self.const: float = const
+        self.const: cython.double = const
 
     @property
-    def name(self) -> str:
+    def name(self) -> cython.char:
         return self.__name
 
-    def evaluate(self, inputs: Dict[FuzzyVariable, float]) -> float:
+    def evaluate(self, inputs: Dict[FuzzyVariable, cython.double]) -> cython.double:
         """
         Calculate linear function
         :param inputs: Values
@@ -76,11 +76,11 @@ class SugenoVariable:
     Sugeno variable
     """
 
-    def __init__(self, name: str, *functions: SugenoFunction):
-        self.name: str = name
+    def __init__(self, name: cython.char, *functions: SugenoFunction):
+        self.name: cython.char = name
         self.functions: List[SugenoFunction] = list(functions)
 
-    def function_by_name(self, name: str) -> SugenoFunction:
+    def function_by_name(self, name: cython.char) -> SugenoFunction:
         for function in self.functions:
             if function.name == name:
                 return function
