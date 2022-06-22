e98b415eed718125dbdc37673fe41d2f73678407
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-22 22:34:05 +0700

    #372 change to same setting as python code of FuzzyLogicToolBox - test, not accurate

--- fuzzy_logic_cpp/test/fuzzy_logic_food_service.cpp
+++ fuzzy_logic_cpp/test/fuzzy_logic_food_service.cpp
@@ -10,48 +10,44 @@ int main(int argc, char *argv[])
     using namespace fl;
     // Code automatically generated with fuzzylite 6.0.
 
-    using namespace fl;
-
     Engine *engine = new Engine;
     engine->setName("service_Service");
     engine->setDescription("");
 
-    InputVariable *food = new InputVariable;
-    food->setName("food");
-    food->setDescription("");
-    food->setEnabled(true);
-    food->setRange(0.000, 10.000);
-    food->setLockValueInRange(false);
-    food->addTerm(new Trapezoid("rancid", 0.000, 0.000, 2.000, 4.000));
-    food->addTerm(new Trapezoid("find", 2.000, 4.000, 6.000, 8.000));
-    food->addTerm(new Trapezoid("delicious", 6.000, 8.000, 10.000, 10.000));
-    engine->addInputVariable(food);
+    InputVariable *input1 = new InputVariable;
+    input1->setName("input1");
+    input1->setDescription("");
+    input1->setEnabled(true);
+    input1->setRange(0.000, 1.000);
+    input1->addTerm(new Triangle("mf1", 0.0, 0.0, 0.5));
+    input1->addTerm(new Triangle("mf2", 0.0, 0.5, 1.0));
+    input1->addTerm(new Triangle("mf3", 0.5, 1.0, 1.0));
+    engine->addInputVariable(input1);
 
-    InputVariable *service = new InputVariable;
-    service->setName("service");
-    service->setDescription("");
-    service->setEnabled(true);
-    service->setRange(0.000, 10.000);
-    service->setLockValueInRange(false);
-    service->addTerm(new Gaussian("poor", 0.000, 5.000));
-    service->addTerm(new Gaussian("good", 5.000, 5.000));
-    service->addTerm(new Gaussian("excellent", 10.000, 5.000));
-    engine->addInputVariable(service);
+    InputVariable *input2 = new InputVariable;
+    input2->setName("input2");
+    input2->setDescription("");
+    input2->setEnabled(true);
+    input2->setRange(0.000, 1.000);
+    input2->addTerm(new Triangle("mf1", 0.0, 0.0, 0.5));
+    input2->addTerm(new Triangle("mf2", 0.0, 0.5, 1.0));
+    input2->addTerm(new Triangle("mf3", 0.5, 1.0, 1.0));
+    engine->addInputVariable(input2);
 
-    OutputVariable *tip = new OutputVariable;
-    tip->setName("tip");
-    tip->setDescription("");
-    tip->setEnabled(true);
-    tip->setRange(0.000, 30.000);
-    tip->setLockValueInRange(false);
-    tip->setAggregation(new Maximum);
-    tip->setDefuzzifier(new Centroid(100));
-    tip->setDefaultValue(fl::nan);
-    tip->setLockPreviousValue(false);
-    tip->addTerm(new Triangle("low", 0.000, 5.000, 15.000));
-    tip->addTerm(new Triangle("average", 5.000, 15.000, 25.000));
-    tip->addTerm(new Triangle("high", 15.000, 25.000, 30.000));
-    engine->addOutputVariable(tip);
+    OutputVariable *output = new OutputVariable;
+    output->setName("output");
+    output->setDescription("");
+    output->setEnabled(true);
+    output->setRange(0.000, 1.000);
+    output->setLockValueInRange(false);
+    output->setAggregation(new Maximum);
+    output->setDefuzzifier(new Centroid(100));
+    output->setDefaultValue(fl::nan);
+    output->setLockPreviousValue(false);
+    output->addTerm(new Triangle("mf1", 0.0, 0.0, 0.5));
+    output->addTerm(new Triangle("mf2", 0.0, 0.5, 1.0));
+    output->addTerm(new Triangle("mf3", 0.5, 1.0, 1.0));
+    engine->addOutputVariable(output);
 
     RuleBlock *mamdani = new RuleBlock;
     mamdani->setName("mamdani");
@@ -61,29 +57,28 @@ int main(int argc, char *argv[])
     mamdani->setDisjunction(new Maximum);
     mamdani->setImplication(new AlgebraicProduct);
     mamdani->setActivation(new General);
-    mamdani->addRule(Rule::parse("if service is poor or food is rancid then tip is low", engine));
-    mamdani->addRule(Rule::parse("if service is good then tip is average", engine));
-    mamdani->addRule(Rule::parse("if service is excellent or food is delicious then tip is high", engine));
+    mamdani->addRule(Rule::parse("if input1 is mf1 and input2 is mf1 then output is mf1", engine));
+    mamdani->addRule(Rule::parse("if input1 is mf2 and input2 is mf2 then output is mf2", engine));
     engine->addRuleBlock(mamdani);
 
     std::string status;
     if (not engine->isReady(&status))
         throw Exception("[engine error] engine is not ready:n" + status, FL_AT);
 
-    scalar food_quality = 7.000;
-    food->setValue(food_quality);
+    scalar input1_val = 0.333;
+    input1->setValue(input1_val);
 
-    scalar service_quality = 8.000;
-    service->setValue(service_quality);
+    scalar input2_val = 0.2;
+    input2->setValue(input2_val);
 
     auto start = high_resolution_clock::now();
     engine->process();
     auto stop = high_resolution_clock::now();
 
-    FL_LOG("food_quality.input = " << Op::str(food_quality) << " and "
-                                   << "service_quality.input = " << Op::str(service_quality)
-                                   << " => "
-                                   << "tip.output = " << Op::str(tip->getValue()));
+    FL_LOG("input1 = " << Op::str(input1_val) << " and "
+                       << "input2 = " << Op::str(input2_val)
+                       << " => "
+                       << "output = " << Op::str(output->getValue()));
 
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
