77cf933c346f92229be64a8fa1779af21a622ae6
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-26 23:45:42 +0700

    #385 calculate fuzzificated set - complete

--- fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/FuzzyVariable.cpp
+++ fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/FuzzyVariable.cpp
@@ -7,9 +7,30 @@ FuzzyVariable::FuzzyVariable(std::string name, float min_value, float max_value)
     this->name = name;
     this->min_value = min_value;
     this->max_value = max_value;
+    this->fuzzificated_set = {};
 }
 
 void FuzzyVariable::addTerm(Term new_term)
 {
     this->list_of_term.push_back(new_term);
+    this->number_of_term = this->list_of_term.size();
+}
+
+void FuzzyVariable::calculateFuzzificatedSet(float value)
+{
+    auto output_value = list_of_term.begin();
+    // std::cout << this->list_of_term.begin() << std::endl;
+    for (int i = 0; i < number_of_term; i++)
+    {
+        this->fuzzificated_set.push_back(output_value->compute(value));
+        std::advance(output_value, 1);
+    }
+}
+
+void FuzzyVariable::printFuzzificatedSet()
+{
+    for (auto output = this->fuzzificated_set.begin(); output != this->fuzzificated_set.end(); ++output)
+    {
+        std::cout << *output << std::endl;
+    }
 }
--- fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/FuzzyVariable.h
+++ fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/FuzzyVariable.h
@@ -4,19 +4,33 @@
 #include <string>
 #include <list>
 #include "Term.h"
+#include <iostream>
 
 namespace FLD
 {
+    enum variable_type
+    {
+        INPUT,
+        OUTPUT,
+        NONE,
+    };
+
     class FuzzyVariable
     {
     public:
-        std::string name;
+        int number_of_term;
         float min_value;
         float max_value;
+        float value;
         std::list<Term> list_of_term;
+        std::string name;
+        std::list<float> fuzzificated_set;
+        variable_type type = NONE;
 
         FuzzyVariable(std::string name, float min_value, float max_value);
         void addTerm(Term new_term);
+        void calculateFuzzificatedSet(float value);
+        void printFuzzificatedSet();
     };
 }
 #endif
