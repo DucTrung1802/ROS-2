a82a36353431196c6f58ec3836f23f7b16c1e9cd
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-26 18:02:00 +0700

    #381 update main.cpp

--- fuzzy_logic_cpp/fuzzy_logic_diy/main.cpp
+++ fuzzy_logic_cpp/fuzzy_logic_diy/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "fuzzy_logic_diy_lib/Term.h"
+#include "fuzzy_logic_diy_lib/FuzzyVariable.h"
 
 using namespace FLD;
 using namespace std;
 
 int main()
 {
-    Term test = Term("rancid", new TriangularMF(0.0, 5.0, 8.0));
+    Term term_1 = Term("mf1", new TriangularMF(0.0, 0.0, 0.5));
+    Term term_2 = Term("mf2", new TriangularMF(0.0, 0.5, 1.0));
+    Term term_3 = Term("mf3", new TriangularMF(0.5, 1.0, 1.0));
+
+    FuzzyVariable input_1 = FuzzyVariable("input1", 0.0, 1.0);
+    input_1.addTerm(term_1);
+    input_1.addTerm(term_2);
+    input_1.addTerm(term_3);
+
     cout << test.compute(6) << endl;
 
     return 0;
