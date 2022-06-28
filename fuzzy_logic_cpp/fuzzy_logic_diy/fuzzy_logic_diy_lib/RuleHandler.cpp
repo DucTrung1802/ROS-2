1a7cc939dc1b11f137ec9528dcb52f7cd3e977ce
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-28 20:02:46 +0700

    #391 working on RuleHandler

--- fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/RuleHandler.cpp
+++ fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/RuleHandler.cpp
@@ -1,7 +1,42 @@
-#include "RuleHandler.h"
 #include <iostream>
+#include <map>
+#include <algorithm>
+#include "RuleHandler.h"
+#include "FuzzyRule.h"
 
 using namespace FLD;
+
+std::string removeSpaces(std::string str)
+{
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
+
+void printListStr(std::list<std::string> const &list)
+{
+    std::cout << "list string: [";
+    for (auto &i : list)
+    {
+        std::cout << " " << i << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+void printNestedList(std::list<std::list<std::string>> const &list)
+{
+    std::cout << "1-layer nested list string: [";
+
+    for (auto &i : list)
+    {
+        std::cout << "[";
+        for (auto &j : i)
+        {
+            std::cout << " " << j << " ";
+        }
+        std::cout << "]";
+    }
+    std::cout << "]" << std::endl;
+}
 
 void eraseSubStr(std::string &mainStr, const std::string &toErase)
 {
@@ -12,6 +47,89 @@ void eraseSubStr(std::string &mainStr, const std::string &toErase)
         // If found then erase it from string
         mainStr.erase(pos, toErase.length());
     }
+}
+
+void sliceString(std::string str, std::string delimiter)
+{
+
+    str = "input1 is mf1 and input2 is mf2";
+    delimiter = " and ";
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = str.find(delimiter)) != std::string::npos)
+    {
+        token = str.substr(0, pos);
+        // each statement except the last statement
+        std::cout << token << std::endl;
+        str.erase(0, pos + delimiter.length());
+    }
+    // the last statement
+    std::cout << str << std::endl;
+}
+
+void RuleHandler::printAntecedentList()
+{
+    printNestedList(this->_antecedent_list);
+}
+
+// support AND only
+std::list<std::list<std::string>> RuleHandler::antecedentHandler(std::string antecedent)
+{
+    // std::cout << antecedent << std::endl;
+
+    // LIST_OF_STATEMENT_BETWEEN_AND
+    std::list<std::string> list_of_statement_between_AND = {};
+
+    // ====== slice ======
+
+    std::string delimiter = " and ";
+    size_t pos = 0;
+    std::string token;
+    while ((pos = antecedent.find(delimiter)) != std::string::npos)
+    {
+        token = antecedent.substr(0, pos);
+        // each statement except the last statement
+        list_of_statement_between_AND.push_back(token);
+        // std::cout << token << std::endl;
+        antecedent.erase(0, pos + delimiter.length());
+    }
+    // the last statement
+    list_of_statement_between_AND.push_back(antecedent);
+
+    // ====== slice ======#
+
+    // std::cout << str << std::endl;
+    // printList(list_of_statement_between_AND);
+
+    // ANTECEDENT_LIST
+
+    for (auto &statement : list_of_statement_between_AND)
+    {
+        // std::cout << statement << std::endl;
+
+        std::list<std::string> temp_list = {};
+        std::string delimiter = " is ";
+        size_t pos = 0;
+        std::string token;
+        while ((pos = statement.find(delimiter)) != std::string::npos)
+        {
+            token = statement.substr(0, pos);
+            // each statement except the last statement
+            temp_list.push_back(token);
+            // std::cout << token << std::endl;
+            statement.erase(0, pos + delimiter.length());
+        }
+        // the last statement
+        statement = removeSpaces(statement);
+        temp_list.push_back(statement);
+        // printListStr(temp_list);
+        _antecedent_list.push_back(temp_list);
+    }
+
+    // printNestedListStr(antecedent_list);
+
+    return _antecedent_list;
 }
 
 void RuleHandler::parseRule(std::string rule)
@@ -33,13 +151,17 @@ void RuleHandler::parseRule(std::string rule)
     // antecedent
     std::string antecedent = rule.substr(0, rule.find(delimiter));
     eraseSubStr(antecedent, "if ");
-    std::cout << "antecedent: " << antecedent << std::endl;
+    // std::cout << "antecedent: " << antecedent << std::endl;
 
     // consequent
     std::string consequent = rule;
     consequent.erase(consequent.begin() + 0, consequent.begin() + consequent.find(delimiter));
     eraseSubStr(consequent, "then ");
-    std::cout << "consequent: " << consequent << std::endl;
+    // std::cout << "consequent: " << consequent << std::endl;
 
-    std::cout << "rule: " << rule << std::endl;
+    // std::cout << "rule: " << rule << std::endl;
+
+    temp_fuzzy_rule.addAntecedentList(antecedentHandler(antecedent));
+
+    // printNestedList(temp_fuzzy_rule.getAntecedentList());
 }
--- fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/RuleHandler.h
+++ fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/RuleHandler.h
@@ -3,22 +3,23 @@
 
 #include <string>
 #include <list>
+#include "FuzzyRule.h"
 
 namespace FLD
 {
-    class FuzzyRule
-    {
-    };
 
     class RuleHandler
     {
     public:
         std::list<std::string> keywords = {"if", "then", "and", "or", "is"};
-
+        std::list<std::list<std::string>> _antecedent_list;
         std::list<FuzzyRule> list_of_fuzzy_rules;
+        FuzzyRule temp_fuzzy_rule;
 
         void parseRule(std::string rule);
         void addRule(FuzzyRule parsed_rule);
+        std::list<std::list<std::string>> antecedentHandler(std::string antecedent);
+        void printAntecedentList();
         bool checkKeyword(std::string word);
     };
 }
