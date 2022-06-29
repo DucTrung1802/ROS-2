a9e36032ecd9b592db219b4afea5f330947062c3
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-06-29 18:18:49 +0700

    #396 current step: adding rule in system

--- fuzzy_logic_cpp/fuzzy_logic_diy/main.cpp
+++ fuzzy_logic_cpp/fuzzy_logic_diy/main.cpp
@@ -2,6 +2,7 @@
 #include "fuzzy_logic_diy_lib/Term.h"
 #include "fuzzy_logic_diy_lib/FuzzyVariable.h"
 #include "fuzzy_logic_diy_lib/RuleHandler.h"
+#include "fuzzy_logic_diy_lib/MamdaniFuzzySystem.h"
 
 using namespace FLD;
 using namespace std;
@@ -11,20 +12,30 @@ int main()
     Term term_1 = Term("mf1", new TriangularMF(0.0, 0.0, 0.5));
     Term term_2 = Term("mf2", new TriangularMF(0.0, 0.5, 1.0));
     Term term_3 = Term("mf3", new TriangularMF(0.5, 1.0, 1.0));
-    Term term_4 = Term("mf4", new TriangularMF(0.75, 1.0, 1.0));
 
     FuzzyVariable input_1 = FuzzyVariable("input1", 0.0, 1.0);
     input_1.addTerm(term_1);
     input_1.addTerm(term_2);
     input_1.addTerm(term_3);
-    input_1.addTerm(term_4);
 
-    input_1.calculateFuzzificatedSet(0.75);
-    // input_1.printFuzzificatedSet();
+    FuzzyVariable input_2 = FuzzyVariable("input2", 0.0, 1.0);
+    input_2.addTerm(term_1);
+    input_2.addTerm(term_2);
+    input_2.addTerm(term_3);
 
-    RuleHandler a_new_rule_handler = RuleHandler();
+    FuzzyVariable output_1 = FuzzyVariable("output1", 0.0, 1.0);
+    output_1.addTerm(term_1);
+    output_1.addTerm(term_2);
+    output_1.addTerm(term_3);
 
-    a_new_rule_handler.parseRule("if input1 is mf1 and input2 is mf2 then output1 is mf2");
+    MamdaniFuzzySystem mamdani_fuzzy_system = MamdaniFuzzySystem({input_1, input_2}, {output_1});
+
+    mamdani_fuzzy_system.addRule("if input1 is mf1 and input2 is mf2 then output1 is mf3");
+
+    mamdani_fuzzy_system.addRule("if input1 is mf2 and input2 is mf2 then output2 is mf3"); // output2 does not exist in system, write antecedentCheck() and consequentCheck()
+
+    std::cout
+        << mamdani_fuzzy_system.getNumberOfFuzzyRule() << std::endl;
 
     return 0;
 }
