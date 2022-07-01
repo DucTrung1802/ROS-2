96149d09c42bd0b77ca2cf57a2441e4673801bef
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-07-01 11:51:16 +0700

    #413 add get, set, print inference set

--- fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/InferenceSet.h
+++ fuzzy_logic_cpp/fuzzy_logic_diy/fuzzy_logic_diy_lib/InferenceSet.h
@@ -14,6 +14,10 @@ namespace FLD
         std::map<std::string, float> mf_set;
 
         InferenceSet(std::string output_var_name, std::map<std::string, float> mf_set);
+        std::string getOutputVarName();
+        void setValueMf(std::string mf_name, float value);
+        float getValueMf(std::string mf_name);
+        void printInferenceSet();
     };
 }
 
