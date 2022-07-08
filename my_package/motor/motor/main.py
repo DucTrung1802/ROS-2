6b60e981fe3dfa3f05bfd1ac4bdc7821bcb347b9
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-07-08 11:00:25 +0700

    #456 initialize ir package

--- my_package/motor/motor/main.py
+++ my_package/motor/motor/main.py
@@ -7,7 +7,7 @@ sum = 0
 
 for i in range(100):
     start = timeit.default_timer()
-    result = calculateFuzzy(42, 0, 42, 0, b"output")
+    result = calculateFuzzy(-40.68, -256.9, 31.82, 3446, b"output")
     end = timeit.default_timer()
     if i == 0:
         print(result[0])
