ae69c71d5c8c7a91f0ec2e6e80e50fe28024ddfd
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-07-10 16:24:08 +0700

    #477 add read_voltage test

--- battery/read_voltage/read_voltage.ino
+++ battery/read_voltage/read_voltage.ino
@@ -1,5 +1,9 @@
 #include "RunningMedian.h"
 
+float VOLTAGE_DIVIDER_FACTOR = 4.1166610794;
+float VOLTAGE_TO_ANALOG_FACTOR = 1.0/ 1240.909;
+float CALIBRATION_FACTOR = 1.0024788723;
+
 RunningMedian samples = RunningMedian(1000);
 
 // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
@@ -10,14 +14,14 @@ float potValue = 0;
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+  delay(100);
 }
 
 void loop() {
   // Reading potentiometer value
-  potValue = analogRead(potPin) / 1.01337737732 * 4.12438 / 1240.909;
+  potValue = analogRead(potPin) * VOLTAGE_DIVIDER_FACTOR * VOLTAGE_TO_ANALOG_FACTOR * CALIBRATION_FACTOR;
   samples.add(potValue);
-  float m = samples.getMedian();
+  float m = samples.getAverage(100);
   Serial.println(m);
   delay(1);
 }
