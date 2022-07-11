8d51afa6def75dbde3e71f5a7fc57fe082f156d5
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-07-12 02:47:22 +0700

    #491 add try for reading oled - IMPORTANT

--- oled/test_oled.py
+++ oled/test_oled.py
@@ -18,6 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from curses import KEY_A1
 import time
 
 import Adafruit_GPIO.SPI as SPI
@@ -44,7 +45,7 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
@@ -54,7 +55,7 @@ disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # Alternatively you can specify an explicit I2C bus number, for example
 # with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_bus=4)
 
 # 128x32 display with hardware SPI:
 # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
@@ -104,28 +105,37 @@ font = ImageFont.load_default()
 
 while True:
 
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    try:
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = subprocess.check_output(cmd, shell=True)
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True)
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True)
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%dGB %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True)
+        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        cmd = "hostname -I | cut -d' ' -f1"
+        IP = subprocess.check_output(cmd, shell=True)
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell=True)
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell=True)
+        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%dGB %s", $3,$2,$5}\''
+        Disk = subprocess.check_output(cmd, shell=True)
 
-    # Write two lines of text.
+        # Write two lines of text.
 
-    draw.text((x, top), "IP: " + str(IP), font=font, fill=255)
-    draw.text((x, top + 8), str(CPU), font=font, fill=255)
-    draw.text((x, top + 16), str(MemUsage), font=font, fill=255)
-    draw.text((x, top + 25), str(Disk), font=font, fill=255)
+        draw.text((x, top), "IP: " + str(IP), font=font, fill=255)
+        draw.text((x, top + 8), str(CPU), font=font, fill=255)
+        draw.text((x, top + 16), str(MemUsage), font=font, fill=255)
+        draw.text((x, top + 25), str(Disk), font=font, fill=255)
 
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(0.1)
+        # Display image.
+        disp.image(image)
+        try:
+            disp.display()
+            time.sleep(0.01)
+
+        except:
+            print("skip 1 time")
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        exit(0)
 
